cba52aea1b8f72522d55583e75c9c1f0f94d8c31
Rebecca J. <Rebecca J.@localhost>
2015-10-15 10:12:41 -0400

    [NEW]Added a map

--- Schooled/Schooled/Source.cpp
+++ Schooled/Schooled/Source.cpp
@@ -2,10 +2,31 @@
 #include <conio.h>
 #include "Console Library/Console.h"
 using namespace std;
-namespace {
-	unsigned const MAP_WIDTH = 20;
-	unsigned const MAP_HEIGHT = 15;
-}
+
+//declaring the map and it's dimensions
+int const MAP_WIDTH = 20;
+int const MAP_HEIGHT = 15;
+int const MAP_FLOOR = 0;
+int const MAP_WALL_TOP = 1;
+int const MAP_DOOR = 2;
+int const MAP_WALL_SIDE = 3;
+int roomOneArray[MAP_HEIGHT][MAP_WIDTH]{
+	{ 3, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 2, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3, 1, 1, 1, 3 },
+	{ 3, 1, 2, 1, 1, 1, 1, 3, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 3, 1, 2, 1, 3, 0, 0, 3, 1, 1, 1, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3 },
+	{ 3, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3 }
+};
 
 class Tile
 {
@@ -16,11 +37,11 @@ private:
 	char character;
 	int colorCode;
 };
+void displayMap();
 
 int main()
 {
 	console.SetTitle("Schooled V0.1");
-
 	// Initialization
 	// Initialize the player's on-screen location
 	int nPlayerX = 40, nPlayerY = 12;
@@ -31,7 +52,7 @@ int main()
 	{
 		// Wipe console clear
 		console.Clear();
-
+		displayMap();
 		// Output phase
 		console.Position(nPlayerX, nPlayerY);
 		console << '8';
@@ -71,7 +92,27 @@ int main()
 		default:
 			break;
 		}
-
+		
 	}
 	return 0;
 }
+void displayMap(){
+	for (int a = 0; a < MAP_HEIGHT; a++){
+		console.Position(0,a);
+		for (int b = 0; b < MAP_WIDTH; b++){
+			if (roomOneArray[a][b] == 0){
+				console << ' ';
+			}
+			else if (roomOneArray[a][b] == 1){
+				console << '_';
+			}
+			else if (roomOneArray[a][b] == 2){
+				console << 'D';
+			}
+			else if (roomOneArray[a][b] == 3){
+				console << '|';
+			}
+		}
+	}
+}
+
