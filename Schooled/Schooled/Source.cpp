dcd92e2efb068e58f6deca11ba44dc4518bb6be8
Rebecca J <36079717+notsoJoly@users.noreply.github.com>
2015-10-18 12:48:50 -0400

    [MERGE] let's hope this fixes things [PROBLEM] still sending code to Graham, don't mind me

---
+++ Schooled/Schooled/Source.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+#include <cassert>
+#include <conio.h>
+#include "Console Library/Console.h"
+using namespace std;
+
+//declaring the map and it's dimensions
+namespace schooled{
+	int const MAP_WIDTH = 60;
+	int const MAP_HEIGHT = 20;
+	int const MAP_FLOOR = 0;
+	int const MAP_WALL_TOP = 1;
+	int const MAP_DOOR = 2;
+	int const MAP_WALL_SIDE = 3;
+	int const ENEMY = 4;
+	int enemy1X = 18;
+	int enemy1Y = 16;
+}
+#define NDEBUG
+
+int roomOneArray[schooled::MAP_HEIGHT][schooled::MAP_WIDTH]{
+	{ 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3 },
+	{ 3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 3, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 1, 1, 2, 1, 3 },
+	{ 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3 },
+	{ 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3 },
+	{ 3, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3 }
+};
+
+struct Tile
+{
+	char character;
+	int colourCode;
+	bool isPassable;
+};
+
+Tile tileIndex[] = {
+	{ ' ', 7, true },	// (0) MAP_FLOOR
+	{ '_', 7, false },	// (1) MAP_WALL_TOP
+	{ 'D', 7, true },	// (2) MAP_DOOR
+	{ '|', 7, false },	// (3) MAP_WALL_SIDE
+	{ 'X', 7, false }   // (4) ENEMY
+};
+
+int message = 0;
+
+
+
+void displayMap();
+void drawTile(int x, int y);
+void enemy1();
+void flavourText();
+bool isPassable(int mapX, int mapY);
+bool isInteractable(int mapX, int mapY);
+
+
+int main()
+{
+	console.SetTitle("Schooled V0.1");
+
+	// Initialization
+	// Initialize the player's on-screen location
+	int nPlayerX = 2, nPlayerY = 19;
+	int nDeltaX, nDeltaY;
+
+	// Main program loop
+	while (true)
+	{
+		// Wipe console clear
+		console.Clear();
+
+		// Output phase
+		displayMap();
+		console.Position(nPlayerX, nPlayerY);
+		console << '8';
+
+		// Input phase
+		KEYPRESS sKeyPress = console.WaitForKeypress();
+
+		// Processing phase
+		switch (sKeyPress.eCode)
+		{
+			// move down
+		case CONSOLE_KEY_DOWN:
+			nDeltaX = 0;
+			nDeltaY = 1;
+			break;
+
+			// move left
+		case CONSOLE_KEY_LEFT:
+			nDeltaX = -1;
+			nDeltaY = 0;
+			break;
+
+			// move right
+		case CONSOLE_KEY_RIGHT:
+			nDeltaX = 1;
+			nDeltaY = 0;
+			break;
+
+			// move up
+		case CONSOLE_KEY_UP:
+			nDeltaX = 0;
+			nDeltaY = -1;
+			break;
+
+			// quit
+		case CONSOLE_KEY_ESCAPE:
+			return 0;
+
+			// Ignore any other key
+		default:
+			break;
+		}
+		//checks to see if anything is interactable
+		if (isInteractable(nPlayerX + nDeltaX, nPlayerY + nDeltaY))
+		{
+			message = 1;
+		}
+		if (message == 1){
+			console.Position(21, 21);
+			console << "Random: What do you want?";
+		}
+		// Check if the player can move in specified direction
+		if (isPassable(nPlayerX + nDeltaX, nPlayerY + nDeltaY))
+		{
+			// If allowed, move in specified direction
+			nPlayerX += nDeltaX;
+			nPlayerY += nDeltaY;
+		}
+
+	}
+	return 0;
+}
+void displayMap(){
+	for (int a = 0; a < schooled::MAP_HEIGHT; a++){
+		console.Position(0,a);
+		for (int b = 0; b < schooled::MAP_WIDTH; b++){
+			drawTile(b, a);
+		}
+	}
+}
+void drawTile(int x, int y)
+{
+	console.Position(x, y);
+	int tile = roomOneArray[y][x];
+	//console.Color(tileIndex[tile].colourCode);
+	console << tileIndex[tile].character;
+}
+
+bool isPassable(int mapX, int mapY){
+	if (mapX < 0 || mapX >= schooled::MAP_WIDTH || mapY < 0 || mapY >= schooled::MAP_HEIGHT)
+		return false;
+
+	int tileValue = roomOneArray[mapY][mapX];
+	if (tileValue == 0)
+		return true;
+	else if (tileValue == 2)
+		return true;
+	else
+		return false;
+}
+bool isInteractable(int mapX, int mapY){
+	int tileValue = roomOneArray[mapY][mapX];
+	if (tileValue == 4)
+		return true;
+	else
+		return false;
+}
