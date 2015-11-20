a4146d4e1a1f9c9282d0b79b2591e7feb7203bf4
Rebecca J <36079717+notsoJoly@users.noreply.github.com>
2015-11-19 20:02:06 -0500

    [CHANGE] some rooms around

---
+++ Schooled/Schooled/Header Files/MenuState.h
@@ -0,0 +1,55 @@
+#ifndef MENUSTATE_H
+#define MENUSTATE_H
+
+#include "GameState.h"
+#include <string>
+#include <vector>
+using std::vector;
+using std::string;
+
+
+
+class MenuState : public GameState
+{
+public:
+	void Init();
+	void Cleanup();
+
+	void Pause();
+	void Resume();
+
+	void HandleEvents(GameEngine* game);
+	void Update(GameEngine* game);
+	void Draw(GameEngine* game);
+
+	static MenuState* Instance() {
+		return &m_MenuState;
+	}
+	static int levelSelected() { return lSelect; }
+
+protected:
+	MenuState() { }
+
+private:
+	static MenuState m_MenuState;
+	string art;
+	int menuSelect, levelSelect;
+	vector<string> menuSelections, levelSelections, 
+		cScheme, dScheme, clScheme, dlScheme;
+	bool selectingControl, selectingLevel, selectingCredits, changedSettings;
+	int selectedControl;
+	static int lSelect;
+
+	void handleMenu(GameEngine* game);
+	void saveSetting(string, string);
+	void initSettings();
+
+	//Gets file contents (NEEDS REMOVAL)
+	string getFileContents(std::ifstream&);
+
+	// Sets up the scheme displays
+	void setSchemes();
+};
+
+
+#endif
