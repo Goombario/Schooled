0bd0cc1ae575777e114de6448ccd1eb6eaa73754
Rebecca J <36079717+notsoJoly@users.noreply.github.com>
2015-11-21 00:43:05 -0500

    [ADD] Principal's room, pricipal door/masterkey system [CHANGE] some text around [FIX] some bugs here and there

---
+++ Schooled/Schooled/Header Files/ShareState.h
@@ -0,0 +1,16 @@
+#ifndef SHARESTATE_H
+#define SHARESTATE_H
+
+#include <vector>
+#include <string>
+using std::string;
+using std::vector;
+
+namespace shared
+{
+	// Get the list of room filenames
+	vector<string> getRoomNames();
+}
+
+
+#endif
